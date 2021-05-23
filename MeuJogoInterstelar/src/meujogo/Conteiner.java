8aedf05f910f8e5459ab82702cb9d93d4afc55b7
Wendeson Farias <wfarias229@gmail.com>
2021-05-23 20:25:27 -0300

    Adiconando o fundo preto na janela

--- MeuJogoInterstelar/src/meujogo/Conteiner.java
+++ MeuJogoInterstelar/src/meujogo/Conteiner.java
@@ -2,10 +2,13 @@ package meujogo;
 
 import javax.swing.JFrame;
 
+import meujogo.Modelo.Fase;
+
 /*criando a tela do jogo*/
 public class Conteiner extends JFrame {
 
 	public Conteiner () {
+		add(new Fase());
 		setTitle("Interstelar");
 		setSize(1024,728);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
